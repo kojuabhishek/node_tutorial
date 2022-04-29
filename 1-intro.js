5bd342f98c955fea5c4cbf07030f017dca1c6c97
kojuabhishek <abhishekkoju@gmail.com>
2022-04-29 21:23:03 +0545

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+const amount = 9 ;
+
+if(amount<10)
+{
+    console.log('Small Number');
+}
+
+else{
+    console.log('Large number');
+}
+
+console.log('Hey, its my first node app');
---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+//GLobals - no window!!!
+
+//__dirname - path to current directory
+//--filename - file name
+//require - function to use modules (CommonJS)
+//module - info about current modelue(file)
+//process - info about enc where the program is being executed
+
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('hello world')
+},1000)
+
